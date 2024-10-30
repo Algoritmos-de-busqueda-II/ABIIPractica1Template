382bf692b67f2742419327adb8a96929972689dc
JavierYuste <javier.yuste@urjc.es>
2024-10-30 13:16:42 +0100

    Added instances

--- src/test/java/AlgorithmGeneralTest.java
+++ src/test/java/AlgorithmGeneralTest.java
@@ -7,11 +7,29 @@ import org.junit.jupiter.api.Assertions;
 import java.io.File;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AlgorithmGeneralTest {
 
+    private static Map<String, Double> expectedQuality = new HashMap<>();
+
+    private static void fillMap(){
+        expectedQuality.put("a_1_n10_m2.txt", 0.0);
+        expectedQuality.put("a_16_n10_m6.txt", 0.0);
+        expectedQuality.put("a_19_n10_m6.txt", 0.0);
+        expectedQuality.put("a_23_n10_m8.txt", 0.0);
+        expectedQuality.put("a_27_n15_m3.txt", 0.0);
+        expectedQuality.put("a_44_n15_m9.txt", 0.0);
+        expectedQuality.put("a_49_n15_m12.txt", 0.0);
+        expectedQuality.put("a_54_n30_m6.txt", 0.0);
+        expectedQuality.put("a_71_n30_m24.txt", 0.0);
+        expectedQuality.put("a_75_n30_m24.txt", 0.0);
+    }
+
     public static void generalTest(String instancePath, Algorithm algorithm, long maxTimePerInstance) {
         try {
+            fillMap();
             File folder = new File(instancePath);
             long numberOfInstances = 0;
             Instant instant = Instant.now();
@@ -22,8 +40,11 @@ public class AlgorithmGeneralTest {
                 Assertions.assertTrue(Evaluator.isFeasible(solution, instance), "La solución no es factible");
                 double score = Evaluator.evaluate(solution, instance);
                 System.out.println("Para la instancia " + fileEntry.getName() +
-                        " el score del algoritmo " + algorithm.toString() + " es " + score);
-                //Assertions.assertTrue(score >= expectedQuality, "La calidad de la solución no es suficiente");
+                        ", la calidad de la solución generada por el algoritmo "
+                        + algorithm.toString() + " es " + score);
+                Assertions.assertTrue(
+                        score >= expectedQuality.getOrDefault(fileEntry.getName(), 0.0),
+                        "La calidad de la solución no es suficiente");
             }
             Duration elapsedTime = Duration.between(instant, Instant.now());
             Assertions.assertTrue(elapsedTime.getSeconds() < (maxTimePerInstance * numberOfInstances),
